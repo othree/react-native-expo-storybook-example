1060ea99570bd9480bdef818100ce2558a8d5ae1
othree <othree@gmail.com>
2020-02-21 17:37:30 +0800

    Export more

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import Button from './components/Button';
+import DateTime from './componets/DateTime';
+import WebView from './componets/WebView';
 
 export Button;
+export DateTime;
+export WebView;
