f6e8d2360abd6e2864f163f37c913d917e4f451c
othree <othree@gmail.com>
2020-02-21 17:50:29 +0800

    Fix export syntax

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import Button from './components/Button';
 import DateTime from './componets/DateTime';
 import WebView from './componets/WebView';
 
-export Button;
-export DateTime;
-export WebView;
+export {
+  Button,
+  DateTime,
+  WebView
+};
